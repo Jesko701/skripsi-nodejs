709ef30ab864570dc2e23b5b71aa29a8f135d345
Jesko701 <IRZHunterBlood@yahoo.com>
2023-11-25 05:48:19 +0700

    fixing primary key on item Child

--- model/Rbac_auth_item_child.js
+++ model/Rbac_auth_item_child.js
@@ -2,11 +2,11 @@ module.exports=(sequelize, DataTypes) => {
     const AuthItemChild = sequelize.define("rbac_auth_item_child", {
         parent: {
             type: DataTypes.STRING,
-            primaryKey: true,
             allowNull: true,
         },
         child: {
             type: DataTypes.STRING,
+            primaryKey: true,
             allowNull: true,
         }
     }, {
